94517eacc4583aadaae544a04bec841c3900d13a
Diego Alejandro Rodrigues Rodriguez <diejorarr@gmail.com>
2025-10-25 23:39:33 -0500

    Adding terminal in the editor and tilemap

--- source/character_body_2d.gd
+++ source/character_body_2d.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
-@export var speed: float = 3.
+@export var walk_speed: float = 3.
 @export var sprint_speed: float = 7.
 
 func _physics_process(_delta: float) -> void:
 	
-	var input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if Input.is_action_pressed("sprint"):
-		position += input*sprint_speed
-	else:
-		position += input*speed
+	var movement_input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
+	var desired_speed: float
+	if Input.is_action_pressed("sprint"):
+		desired_speed = sprint_speed
+	else:
+		desired_speed = walk_speed
+	
+	# I think is better to use the functions that CharacterBody2D gives to move the player
+	velocity = desired_speed * movement_input
+	move_and_slide()
