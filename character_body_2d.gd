e65e20ce78a3224b94cf6ddf89de8a844b5f1578
Huylium <117189377+Huylium@users.noreply.github.com>
2025-10-25 19:40:29 -0500

    firstChange!!
    added sprint
    added menu template
    added sprite template(wolf)

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 
-@export var speed = 25.
+@export var speed = 3.
 
 func _physics_process(_delta: float) -> void:
 	
 	var input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	position += input*speed
+	var ifSprint = 0.
+	if Input.is_action_pressed("run_Toggle"):
+		ifSprint = 7.
+	else:
+		ifSprint = 0.	
+	var speedHolder = ifSprint+ speed
+	position += input*speedHolder
+	
