f15b7baefe6e3368d05ac78db80a992f5cf821ec
Diego Rodrigues <diejorarr@gmail.com>
2025-10-30 17:07:54 -0500

    Adding websockets support

--- source/networking/client/client.gd
+++ source/networking/client/client.gd
@@ -4,7 +4,7 @@ extends Node
 @export var port = 21253
 
 var multiplayer_api := SceneMultiplayer.new()
-var multiplayer_peer := ENetMultiplayerPeer.new()
+var multiplayer_peer := WebSocketMultiplayerPeer.new()
 
 
 func _ready():
@@ -12,26 +12,32 @@ func _ready():
 		process_mode = Node.PROCESS_MODE_DISABLED
 		return
 
+	multiplayer_api.peer_connected.connect(on_peer_connected)
 
-func create_client(server_address: String):
-	multiplayer_peer.create_client(server_address, port)
+func init(server_address: String):
+	var cert := X509Certificate.new()
+	var cert_path = "res://server_cert.crt"
+	cert.load(cert_path)
+	var tls_client_options := TLSOptions.client(cert)
+	var creation_result = multiplayer_peer.create_client("wss://" + server_address + ":" + str(port), tls_client_options)
+	
+	if creation_result != OK:
+		push_warning("Can't create client: " + str(creation_result))
+		return creation_result
 		
+
+	config_api()
+	print("Client connecting to ", server_address, ":", port)
+
+func on_peer_connected(peer_id: int) -> void:
+	var msg = "Hello to client %d, by: %d" % [peer_id, multiplayer_peer.get_unique_id()]
+	$TestRPC.rpc_send_message.rpc_id(peer_id, msg)
+
+
+func config_api():
 	multiplayer_api.multiplayer_peer = multiplayer_peer
 	multiplayer_api.root_path = get_path()
 	get_tree().set_multiplayer(multiplayer_api, get_path())
-	
-	set_multiplayer_authority(multiplayer_peer.get_unique_id())
-	$Players/PlayerSpawner.set_multiplayer_authority(1)
-	var client_msg = "Client (%d)" % get_multiplayer_authority()
-	print(client_msg + " connecting to ", server_address, ":", port)
-
-	multiplayer_api.peer_connected.connect(on_peer_connected)
-
-
-func on_peer_connected(peer_id: int) -> void:
-	var msg = "Hello to client %d, by: %d" % [peer_id, get_multiplayer_authority()]
-	$TestRPC.rpc_send_message.rpc_id(peer_id, msg)
-
 
 # We need to manually poll because we are overwriting the MultiplayerAPI of the root.
 func _process(_delta: float):
--- source/networking/server/server.gd
+++ source/networking/server/server.gd
@@ -4,31 +4,34 @@ extends Node
 @export var port = 21253
 
 var multiplayer_api := SceneMultiplayer.new()
-var multiplayer_peer := ENetMultiplayerPeer.new()
+var multiplayer_peer := WebSocketMultiplayerPeer.new()
+
+@onready var crypto := Crypto.new()
+@onready var tls_key := crypto.generate_rsa(2048)
+@onready var cert := crypto.generate_self_signed_certificate(tls_key, "CN=localhost,O=MyOrganisation,C=US")
 
 func _ready():
 	if "--server" in OS.get_cmdline_args():
-		create_server()
-
-func create_server() -> Error:
-	var creation_result: Error = multiplayer_peer.create_server(port)
+		init()
 	
-	# Early exit if peer can't create server, most likely because port is not available
-	if creation_result == ERR_CANT_CREATE:
-		return creation_result
+	var cert_path = "res://server_cert.crt"
+	cert.save(cert_path)
 	
-	multiplayer_api.root_path = get_path()
-	multiplayer_api.multiplayer_peer = multiplayer_peer
-	
-	get_tree().set_multiplayer(multiplayer_api, get_path())
-	
-	print("Server ready on ", "*", ":", port)
-
 	multiplayer_api.peer_connected.connect(on_peer_connected)
 	multiplayer_api.peer_disconnected.connect(on_peer_disconnected)
+
+func init() -> Error:
+	var tls_server_options: TLSOptions = TLSOptions.server(tls_key, cert)
+	var creation_result: Error = multiplayer_peer.create_server(port, "*", tls_server_options)
+	
+	if creation_result == ERR_CANT_CREATE:
+		push_warning("Most likely port is not available.")
+		return creation_result
+	
+	config_api()
+	print("Server ready on ", "*", ":", port)
 	
 	return creation_result
-
 
 func on_peer_connected(peer_id: int) -> void:
 	var msg = "Hello to client %d, by: %d" % [peer_id, multiplayer_peer.get_unique_id()]
@@ -41,6 +44,11 @@ func on_peer_disconnected(peer_id: int) -> void:
 		if int(child.name) == peer_id:
 			child.queue_free()
 
+
+func config_api():
+	multiplayer_api.multiplayer_peer = multiplayer_peer
+	multiplayer_api.root_path = get_path()
+	get_tree().set_multiplayer(multiplayer_api, get_path())
 
 # We need to manually poll because we are overwriting the MultiplayerAPI of the root.
 func _process(_delta: float):
