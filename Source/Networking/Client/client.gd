bdad66b31ae990ed7d276abc0fd0bf600cc9f8a5
Diego Alejandro Rodrigues Rodriguez <diejorarr@gmail.com>
2025-11-23 21:24:20 -0600

    Spawn with SaveComponent

--- Source/Networking/Client/client.gd
+++ Source/Networking/Client/client.gd
@@ -22,15 +22,10 @@ func _ready() -> void:
 	multiplayer_api.connected_to_server.connect(on_connected_to_server)
 
 func init(server_address: String, _username: String) -> Error:
+	_reset_peer_state()
 	var connection_code: Error = create_connection(server_address, _username)
 	if connection_code == OK:
-		# Fixes a weird bug where `MultiplayerSynchronizers` think they are connected
-		# to the server when they are offline.
-		for synchronizer in get_tree().get_nodes_in_group("synchronizers"):
-			synchronizer.queue_free()
-		while not get_tree().get_nodes_in_group("synchronizers").is_empty():
-			await get_tree().create_timer(0.01).timeout
-		
+		_clear_synchronizers()
 		config_api()
 		
 	return connection_code
@@ -41,7 +36,7 @@ func create_connection(server_address: String, _username: String) -> Error:
 
 	var err: Error = multiplayer_peer.create_client(url)
 	if err != OK:
-		push_warning("Can't create client (%s) to %s" % [err, url])
+		push_warning("Can't create client (%s) to %s" % [error_string(err), url])
 		return err
 	
 	print("Client connecting to ", url)
@@ -49,10 +44,13 @@ func create_connection(server_address: String, _username: String) -> Error:
 	return OK
 
 func build_url(server_address: String) -> String:
+	if server_address.is_empty():
+		return "wss://" + public_host
+
 	if server_address == "localhost" or server_address == "127.0.0.1":
 		return "ws://localhost:" + str(port)
 
-	return "wss://" + public_host
+	return "wss://" + server_address
 
 func on_peer_connected(peer_id: int) -> void:
 	peer_connected.emit(peer_id)
@@ -63,10 +61,31 @@ func on_connected_to_server() -> void:
 	connected_to_server.emit()
 
 func config_api() -> void:
+	assert(SceneManager, "SceneManager autoload must exist before configuring the client.")
+	var scene_root: NodePath = SceneManager.get_path()
+	assert(scene_root != NodePath(""), "SceneManager path must be valid before client configuration.")
+
 	multiplayer_api.multiplayer_peer = multiplayer_peer
-	multiplayer_api.root_path = SceneManager.get_path()
-	get_tree().set_multiplayer(multiplayer_api, SceneManager.get_path())
+	multiplayer_api.root_path = scene_root
+	get_tree().set_multiplayer(multiplayer_api, scene_root)
 
 func _process(_dt: float) -> void:
-	if multiplayer_api.has_multiplayer_peer():
+	if multiplayer_api.has_multiplayer_peer() and multiplayer_api.multiplayer_peer is not OfflineMultiplayerPeer:
 		multiplayer_api.poll()
+
+func _reset_peer_state() -> void:
+	if multiplayer_api.has_multiplayer_peer() and multiplayer_api.multiplayer_peer is WebSocketMultiplayerPeer:
+		var previous_peer: WebSocketMultiplayerPeer = multiplayer_api.multiplayer_peer
+		if previous_peer.get_connection_status() != MultiplayerPeer.CONNECTION_DISCONNECTED:
+			previous_peer.close()
+
+	multiplayer_api.multiplayer_peer = OfflineMultiplayerPeer.new()
+	multiplayer_peer = WebSocketMultiplayerPeer.new()
+
+func _clear_synchronizers() -> void:
+	# Fixes a weird bug where `MultiplayerSynchronizers` think they are connected
+	# to the server when they are offline.
+	for synchronizer in get_tree().get_nodes_in_group("synchronizers"):
+			synchronizer.queue_free()
+	while not get_tree().get_nodes_in_group("synchronizers").is_empty():
+		await get_tree().create_timer(0.01).timeout
--- Source/Networking/Server/server.gd
+++ Source/Networking/Server/server.gd
@@ -15,36 +15,41 @@ const lobbies_scene: PackedScene = preload("res://Source/Networking/Server/Lobbi
 
 func _ready() -> void:
 	add_child(lobbies)
-	if OS.has_feature("web"):
-		return
-	if "--server" in OS.get_cmdline_args():
-		init()
-
 	multiplayer_api.peer_connected.connect(on_peer_connected)
 	multiplayer_api.peer_disconnected.connect(on_peer_disconnected)
 
 func init() -> Error:
-	var err := multiplayer_peer.create_server(port)
+	assert(not OS.has_feature("web"),
+		"Server.init() cannot run on web builds: %s" % error_string(ERR_UNAVAILABLE))
+
+	multiplayer_peer = WebSocketMultiplayerPeer.new()
+	var err: Error = multiplayer_peer.create_server(port)
 	if err != OK:
-		push_warning("create_server failed: %s" % str(err))
+		push_warning("create_server failed: %s" % error_string(err))
 		return err
+
 	config_api()
 	print("Server ready on *:%d" % port)
-	
+
 	return OK
 
 func on_peer_connected(peer_id: int) -> void:
 	peer_connected.emit(peer_id)
+	say_hi.rpc_id(peer_id)
 
 func on_peer_disconnected(peer_id: int) -> void:
 	peer_disconnected.emit(peer_id)
 
 func config_api() -> void:
+	assert(is_instance_valid(lobbies), "Server lobbies node is missing before configuration.")
 	multiplayer_api.multiplayer_peer = multiplayer_peer
 	multiplayer_api.root_path = lobbies.get_path()
 	get_tree().set_multiplayer(multiplayer_api, lobbies.get_path())
-	#get_tree().set_multiplayer(multiplayer_api)
 
 func _process(_dt: float) -> void:
 	if multiplayer_api.has_multiplayer_peer():
 		multiplayer_api.poll()
+
+@rpc
+func say_hi() -> void:
+	print("hi from: ", get_multiplayer_authority())
