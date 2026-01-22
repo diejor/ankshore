323c83587056f4b4849f31047eab185e6f34de83
Diego Alejandro Rodrigues Rodriguez <diejorarr@gmail.com>
2026-01-22 15:51:56 -0600

    Some changes

--- source/scenes/areas/scene/scene_manager_autoload.gd
+++ source/scenes/areas/scene/scene_manager_autoload.gd
@@ -1,11 +1,18 @@
 extends Node
 
 @onready var transition_anim: AnimationPlayer = Client.scene_manager.get_node("%TransitionAnim")
+@onready var transition_progress: TextureProgressBar = Client.scene_manager.get_node("%TransitionProgress")
+@onready var tp_canvas_layer: CanvasLayer =  Client.scene_manager.get_node("%TPCanvasLayer")
+
+func _ready() -> void:
+	tp_canvas_layer.set_deferred("visible", false)
 
 func teleport(
 	username: String, 
 	from_scene_name: String, 
 	tp_path: String) -> void:
+	transition_anim.play("show")
+	
 	Client.scene_manager.request_teleport.rpc_id(
 		MultiplayerPeer.TARGET_PEER_SERVER,
 		username,
