84fe65c8b043a5b485aa7a20e8b17205c0936d88
Diego Alejandro Rodrigues Rodriguez <diejorarr@gmail.com>
2026-03-01 01:36:23 -0600

    More changes before having to go

--- source/combat/characterStuff/Characters/testCharacterFolder/test_character.gd
+++ source/combat/characterStuff/Characters/testCharacterFolder/test_character.gd
@@ -6,14 +6,17 @@ class_name testCharacter extends Node2D
 @export var stats: charStats = charStats.new()
 
 #fuck
-var moveList: Array[charAction] = []
+@export var moveList: Array[charAction] = []
 #have set of moves instatiated here
 #moveList.push_front(test_attack_move_1) #doesnt work
 
+@warning_ignore("unused_signal")
 signal health_depleted
+@warning_ignore("unused_signal")
 signal will_depleted
+@warning_ignore("unused_signal")
 signal courage_depleted
-signal transferStats(health: int, dmg: int, will: int, defense: int, blockingDefense: int, courage: int)
+signal transferStats(stats: charStats)
 #transferStats.emit(stats.health, stats.damageStat, stats.will, stats.defense, stats.blockingDefense, stats.courage)
 #signal transferStats(dmg: int)
 #health
@@ -23,11 +26,15 @@ signal transferStats(health: int, dmg: int, will: int, defense: int, blockingDef
 #blockingDefense
 #courage
 
-
+func get_attacks() -> Array:
+	var filter_attacks := func(action: charAction) -> bool:
+		return action is charAttack
+	
+	return moveList.filter(filter_attacks)
 
 ### gameplay Stuff
 func recalculateMoves() -> void:
-	transferStats.emit(stats.health, stats.damageStat, stats.will, stats.defense, stats.blockingDefense, stats.courage)
+	transferStats.emit(stats)
 	
 func takeDmg(rawDamage: int, blocked: bool) -> void:
 	stats.damageTaken(rawDamage, blocked)
--- source/combat/turn_manager.gd
+++ source/combat/turn_manager.gd
@@ -2,3 +2,22 @@ class_name turnManager extends Node
 signal turnEnded
 signal turnStart
 signal matchStart
+
+enum Teams {
+	Ally,
+	Enemy
+}
+
+var current_team: Teams = Teams.Ally
+
+@onready var turn_label: DebugLabel = %TurnLabel
+
+func _ready() -> void:
+	pass
+
+func update_turn_label() -> void:
+	var label_text: String = "Turn %s"
+	
+
+func next_turn() -> void:
+	pass
