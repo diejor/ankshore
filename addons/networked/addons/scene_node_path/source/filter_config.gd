dd5ae2ce391fff696c6d6c01236b2d6a8e2db288
Diego Alejandro Rodrigues Rodriguez <diejorarr@gmail.com>
2026-05-19 21:39:20 -0500

    removing networked

--- addons/networked/addons/scene_node_path/source/filter_config.gd
+++
@@ -1,8 +0,0 @@
-## Configuration for filtering nodes in the [SceneNodePath] picker dialog.
-class_name FilterConfig
-extends RefCounted
-
-var target_class: String = "Node"
-var is_builtin: bool = true
-var custom_script: Script = null
-var icon: Texture2D = null
--- addons/networked/debug/ui/debug_label.gd
+++
@@ -1,6 +0,0 @@
-## Simple debug label that automatically frees itself in non-debug builds.
-class_name DebugLabel
-extends Label
-
-func _init() -> void:
-	DebugFeature.free_if_debug(self)
